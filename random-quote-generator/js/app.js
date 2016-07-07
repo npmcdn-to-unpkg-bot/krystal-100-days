7f37016bfed4df7b895ebc04f26e722e592bee62
kklumpp13 <kklumpp13@gmail.com>
2016-07-06 21:56:29 -0400

    Create basic template and add user stories

---
+++ random-quote-generator/js/app.js
@@ -0,0 +1,11 @@
+//I can click a button to show a new random quote
+  //Model
+    //Array of objects with quotes
+  //Controller
+  
+  //View
+    //Render random quote on button click
+
+
+
+//I can press a button to tweet out a quote
