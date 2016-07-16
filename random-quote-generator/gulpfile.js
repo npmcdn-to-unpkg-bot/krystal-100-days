604f56ce44be95712cfc16e12dc0c5865c5f0184
kklumpp13 <kklumpp13@gmail.com>
2016-07-15 21:31:39 -0400

    Add plumber for error handling and sourcemaps for debugging

--- random-quote-generator/gulpfile.js
+++ random-quote-generator/gulpfile.js
@@ -4,6 +4,9 @@ var livereload = require('gulp-livereload');
 var concat = require('gulp-concat');
 var minifyCss = require('gulp-minify-css');
 var autoprefixer = require('gulp-autoprefixer');
+var plumber = require('gulp-plumber');
+var sourcemaps = require('gulp-sourcemaps');
+
 
 // File paths
 var DIST_PATH = 'public/dist';
@@ -14,9 +17,16 @@ var CSS_PATH = 'public/css/**/*.css';
 gulp.task('styles', function() {
   console.log('starting styles task');
   return gulp.src(['public/css/reset.css', CSS_PATH])
+    .pipe(plumber(function(err) {
+      console.log('Styles Task Error');
+      console.log(err);
+      this.emit('end');
+    }))
+    .pipe(sourcemaps.init())
     .pipe(autoprefixer())
     .pipe(concat('styles.css'))
     .pipe(minifyCss())
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest(DIST_PATH))
     .pipe(livereload());
 });
