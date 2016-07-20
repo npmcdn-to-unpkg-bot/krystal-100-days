ae38632d483ace51a3f80541add65a1d3221af88
kklumpp13 <kklumpp13@gmail.com>
2016-07-19 21:26:04 -0400

    Clean formatting and remove extra console.logs

--- exercises-for-programmers/0. tip-calculator/app.js
+++ exercises-for-programmers/0. tip-calculator/app.js
@@ -2,8 +2,9 @@ function tipCalculator() {
   var billAmount = 0,
     tip = 0,
     tipRate = 0,
+    tipDollars = 0,
     total = 0;
-  
+
   getBillAmount();
   getTipAmount();
   calculateTip();
@@ -11,55 +12,20 @@ function tipCalculator() {
 
 function getBillAmount() {
   billAmount = prompt("What is the bill amount?") * 100;
-  return billAmount;
 }
 
 function getTipAmount() {
   tipRate = prompt("What is the tip rate?") / 100;
-  return tipRate;
 }
 
 function calculateTip() {
   tip = billAmount * tipRate;
-  tipDollars = (tip/100).toFixed(2);
-  
+  tipDollars = (tip / 100).toFixed(2);
+
   total = ((billAmount + tip) / 100).toFixed(2);
 
   console.log("Tip: $" + tipDollars);
   console.log("Total: $" + total);
 }
 
-
-
 tipCalculator();
-
-
-
-
-
-
-
-
-//Tests
-
-//1.
-  //bill amount: 10
-  //tip rate: 15
-  //expected: 
-    //tip: $1.50
-    //Total: $11.50
-
-//2. 
-  //bill amount: 11.25
-  //tip rate: 15
-  //expected: 
-    //tip: $1.69
-    //total: $12.94
-
-//3. 
-  //bill amount: 12
-  //tip rate: 0
-    //expected: 
-      //tip: 0
-      //total: 12
-
