f761187f5c5a153c1d7b6f5586b152c386530dcf
kklumpp13 <kklumpp13@gmail.com>
2016-07-22 22:56:14 -0400

    Create model logic to store ship data

--- battleship/public/js/app.js
+++ battleship/public/js/app.js
@@ -1,3 +1,54 @@
+var model = {
+  boardSize: 7,
+  numShips: 3,
+  shipLength: 3,
+  shipsSunk: 0,
+  ships: [
+    {
+      locations: ["10", "20", "30"],
+      hits: ["", "", ""]
+    },
+    {
+      locations: ["32", "33", "34"],
+      hits: ["", "", ""]
+    },
+    {
+      locations: ["63", "64", "65"],
+      hits: ["", "", ""]
+    }
+  ],
+  fire: function(guess) {
+    for (var i = 0; i < this.numShips; i++) {
+      var ship = this.ships[i];
+      var index = ship.locations.indexOf(guess);
+      if (index >= 0) {
+        ship.hits[index] = "hit";
+        view.displayHit(guess);
+        view.displayMessage("HIT!");
+        if (this.isSunk(ship)) {
+          view.displayMessage("You sank my battleship");
+          this.shipsSunk++;
+        }
+        return true;
+      }
+    }
+    view.displayMiss(guess);
+    view.displayMessage("You missed.");
+    //Indicate a miss
+    return false;
+  },
+  isSunk: function(ship) {
+    for(var i = 0; i < this.shipLength; i++) {
+      if(ship.hits[i] !== "hit") {
+        //ship is still floating
+        return false;
+      }
+    }
+//    return true;
+  }  
+  
+};
+
 var view = {
   displayMessage: function(msg) {
     //Use DOM to get #messageArea
@@ -24,9 +75,14 @@ var view = {
   }
 };
 
-view.displayMiss("00");
-view.displayHit("34");
-view.displayMiss("55");
-view.displayHit("26");
-view.displayMessage("Is this working?");
+//view.displayMiss("00");
+//view.displayHit("34");
+//view.displayMiss("55");
+//view.displayHit("26");
+//view.displayMessage("Is this working?");
+model.fire("53");
+model.fire('06');
+model.fire("16");
+model.fire("63");
+model.fire("64");
 
