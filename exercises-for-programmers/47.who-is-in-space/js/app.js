82f0a3b06802779a9aeac2cb47f5115e311c8952
kklumpp13 <kklumpp13@gmail.com>
2016-07-30 21:56:02 -0400

    Create output for API to list astronauts by name and craft

--- exercises-for-programmers/47.who-is-in-space/js/app.js
+++ exercises-for-programmers/47.who-is-in-space/js/app.js
@@ -8,6 +8,26 @@ $(document).ready(function(){
     console.log(response)
     var people = response.people;
     console.log(people)
+    
+    var spaceHTML = '<ul class="output">';
+    spaceHTML += '<li><div class="th">' + "Name" + '</div>';
+    spaceHTML += '<div class="th">' + "Craft" + '</div></li>';
+    
+    for (var i = 0; i < people.length; i++) {
+      var details = people[i];
+      var name = details.name;
+      var craft = details.craft;
+      
+       spaceHTML += '<li>';
+       spaceHTML += '<div class="name">' + name + '</div>';
+       spaceHTML += '<div class="craft">' + craft + '</div>';
+       spaceHTML += '</li>';
+      
+    }
+    spaceHTML += '</ul>';
+    $("#outerSpace").append(spaceHTML);
+    
+    
   })  
 });
 
