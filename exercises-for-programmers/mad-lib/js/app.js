1e507e134a9320325696dd318c105029f2fca4f5
kklumpp13 <kklumpp13@gmail.com>
2016-08-02 21:56:01 -0400

    Complete simple mad lib game

---
+++ exercises-for-programmers/mad-lib/js/app.js
@@ -0,0 +1,14 @@
+//Enter a noun
+//Enter a verb
+//Enter an adjective
+//Enter an adverb
+//Story
+
+var noun = prompt("Enter a noun");
+var verb = prompt("Enter a verb");
+var adj = prompt("Enter an adjective");
+var adv = prompt("Enter an adverb");
+
+
+var story = "Do you " + verb + " your " + adj + " " + noun + " " + adv;
+console.log(story);
