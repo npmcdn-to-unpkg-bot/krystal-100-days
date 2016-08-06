f24de6a8b03eaf762bdbf56bde48977d5ff6ab05
kklumpp13 <kklumpp13@gmail.com>
2016-08-05 23:24:53 -0400

    Add state to inputs

--- exercises-for-programmers/0.5 react-simple-math/src/components/number-input.js
+++ exercises-for-programmers/0.5 react-simple-math/src/components/number-input.js
@@ -1,24 +1,42 @@
 import React, { Component } from 'react';
 
 class NumInput extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      numOne: '',
+      numTwo: ''
+    };
+  }
+
+
   render() {
     return (
       <div> 
         <div className="input">
           <label htmlFor="numOne">What is your first number?</label>
-          <input type="text" id="numOne" onChange={this.onInputChange}/>
+          <input type="text" id="numOne" 
+            value={this.state.numOne}
+            onChange={event => this.setState({numOne: event.target.value})}/>
+            Value is: {this.state.numOne}
         </div>  
         <div className="input">
           <label htmlFor="numTwo">What is your second number?</label>
-          <input type="text" onChange={this.onInputChange}/>
+          <input type="text" 
+            value={this.state.numTwo}
+            onChange={event => this.setState({numTwo: event.target.value})}/>
+            Value is: {this.state.numTwo}
         </div>
       </div>
     )
   }
 
-  onInputChange = (event) => {
-    console.log(event.target.value)
-  }
+  // onInputChange = (event) => {
+  //   this.setState({
+  //     numOne: 
+  //   })
+  // }
 
 };
 
