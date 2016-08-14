02fec01d1dc0239fac0dbd0fac8da1d9b90d0872
kklumpp13 <kklumpp13@gmail.com>
2016-08-13 21:12:15 -0400

    Add test for petfinder API

---
+++ petFinder/src/components/api-test.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+  console.log(0);
+
+// http://api.petfinder.com/pet.find?format=json&key=3a62ece31719a64dcf6726980917d7ad&animal=dog&location=94089&age=senior&count=10
+  var petURL = "http://api.petfinder.com/pet.find?format=json&key=" + API_KEY + "&animal=dog&location=33433";
+  console.log(petURL)
+
+  $.ajax({
+    type: 'GET',
+    url: petURL + '&callback=?',
+    dataType: "jsonp",
+  }).done(function(response) {
+    console.log(response.petfinder.pets.pet);
+    var petArray = response.petfinder.pets.pet;
+    console.log(petArray)
+    for (var i = 0; i < petArray.length; i++) {
+      var petType = petArray[i];
+      var petName = petType.name['$t'];
+      console.log(petName)
+      var petBreed = petType.breeds.breed['$t'];
+      console.log(petBreed)
+    }
+
+
+  }).error(function(err) {
+    console.log('Error retreiving data');
+  });
+});
